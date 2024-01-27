36f03c9809756f088b531ec47fe8a6cba2073bfd
fobsorlando <orlandosaboia@gmail.com>
2024-01-26 22:04:11 -0300

    atualizando

--- src/app.ts
+++ src/app.ts
@@ -8,22 +8,13 @@ import { sendWhatsMessage } from './services/twilio';
 
 
 const app = express();
-// const port = 3000;Z
 const port = process.env.PORT || 3500;
 
 
-//app.use(bodyParser.urlencoded())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-//const allowedOrigins = ['http://localhost:3500'];
-
-//const options: cors.CorsOptions = {
-//  origin: allowedOrigins
-//};
-
-//app.use(cors(options));
 
 dotenv.config()
 
@@ -33,7 +24,6 @@ dotenv.config()
 
   try {
     const result = await sendWhatsMessage(`whatsapp:${to}`,body)
-    console.log(result)
     res.status(200).json({success: true, body})
   }catch  (error) { 
     res.status(500).json({success: false,error})
--- src/services/twilio.ts
+++ src/services/twilio.ts
@@ -8,14 +8,9 @@ const TOKEN =process.env.TWILIO_AUTH_TOKEN
 const PHONE = process.env.PHONE
 
 const client = twilio(SID,TOKEN)
-//const to='whatsapp:+558699818253'
-//const body='teste dos teste'
 
-console.log('vEJA: ',PHONE)
-console.log(SID,'--',TOKEN)
              
 export const sendWhatsMessage = async (to: string, body: string ): Promise<void> => {
-    console.log('Olhe:',to)
     try {
         await client.messages.create ({
             to: to,
